624167034f2b1d97356c6f6f3d89cce02e593f99
malaudiaz <113136614+malaudiaz@users.noreply.github.com>
2025-07-30 09:35:16 -0400

    update customer & recipient

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 // prisma/seed.ts
-import { PrismaClient } from '@prisma/client';
-import dotenv from 'dotenv';
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
 import bcrypt from "bcryptjs";
 
 // Cargar variables de entorno
@@ -29,28 +29,42 @@ async function main() {
   // Por ejemplo:
   await prisma.$transaction(async (tx) => {
     // Operaciones dentro de la transacción
-   
-    const user = await tx.user.create({
-      data: {
-        name: 'Miguel',
-        email: 'miguel@example.com', 
-        password: await bcrypt.hash("123456", 10),
-        role: 'ADMIN',
-      },
+
+    await tx.user.createMany({
+      data: [
+        {
+          name: "Miguel",
+          email: "miguel@example.com",
+          password: await bcrypt.hash("123456", 10),
+          role: "ADMIN",
+        },
+        {
+          name: 'Robert',
+          email: 'robert@example.com', 
+          password: await bcrypt.hash("123456", 10),
+          role: 'USER',
+        },
+      ],
     });
 
-    const country =await tx.country.create({
+    const user = await tx.user.findFirst();
+
+    if (!user) {
+      throw new Error("No se pudo crear el usuario");
+    }
+
+    const country = await tx.country.create({
       data: {
-        name: 'Mexico',
-        code: 'MX',
+        name: "Mexico",
+        code: "MX",
         createdById: user.id,
       },
     });
-  
+
     const state = await tx.state.create({
       data: {
-        name: 'Jalisco',
-        code: 'JAL',
+        name: "Jalisco",
+        code: "JAL",
         countryId: country.id,
         createdById: user.id,
       },
@@ -58,8 +72,8 @@ async function main() {
 
     const city = await tx.city.create({
       data: {
-        name: 'Guadalajara',
-        code: 'GUA',
+        name: "Guadalajara",
+        code: "GUA",
         stateId: state.id,
         createdById: user.id,
       },
@@ -67,11 +81,11 @@ async function main() {
 
     await tx.systemSetting.create({
       data: {
-        name: 'My Company',
-        code: '1001',
-        zipCode: '10300',
+        name: "My Company",
+        code: "1001",
+        zipCode: "10300",
         numCustomerPercentRate: 0.5,
-        customerPercentRate: 1.00,
+        customerPercentRate: 1.0,
         moneyOrderFeed: 0.49,
         maxBankDepositLimit: 100,
         minimunAge: 5,
@@ -83,18 +97,18 @@ async function main() {
     await tx.company.createMany({
       data: [
         {
-          name: 'Wire',
-          description: 'Wire Transfer',
+          name: "Wire",
+          description: "Wire Transfer",
           createdById: user.id,
         },
         {
-          name: 'Vigo',
-          description: 'Banco Vigo',
+          name: "Vigo",
+          description: "Banco Vigo",
           createdById: user.id,
         },
         {
-          name: 'WesterUnion',
-          description: 'Compañia WesterUnion',
+          name: "WesterUnion",
+          description: "Compañia WesterUnion",
           createdById: user.id,
         },
       ],
@@ -103,94 +117,93 @@ async function main() {
     await tx.customer.createMany({
       data: [
         {
-          code: '8938',
-          firstName: 'Antonio',
-          middleName: 'Jose',
-          lastNameOne: 'Perez',
-          lastNameTwo: 'Sanchez',
-          address: 'Calle 123',
-          apartment: '23',
+          code: "8938",
+          firstName: "Antonio",
+          middleName: "Jose",
+          lastNameOne: "Perez",
+          lastNameTwo: "Sanchez",
+          address: "Calle 123",
+          apartment: "23",
           countryId: country.id,
           stateId: state.id,
           cityId: city.id,
-          zipCode: '10300',
-          phone: '1001001001',
-          dob: new Date('1990-01-01'),
-          ssn: '123456789',
-          dlid: '123456789',
-          imageUrl: '',
+          zipCode: "10300",
+          phone: "1001001001",
+          dob: new Date("1990-01-01"),
+          ssn: "123456789",
+          dlid: "123456789",
+          imageUrl: "",
           percentage: 0,
           createdById: user.id,
-          type: 'CUSTOMER',
-          notes: '',
+          type: "CUSTOMER",
+          notes: "",
         },
         {
-          code: '8939',
-          firstName: 'Juan',
-          middleName: 'Carlos',
-          lastNameOne: 'Rodriguez',
-          lastNameTwo: 'Diaz',
-          address: 'Calle 19',
-          apartment: '32',
+          code: "8939",
+          firstName: "Juan",
+          middleName: "Carlos",
+          lastNameOne: "Rodriguez",
+          lastNameTwo: "Diaz",
+          address: "Calle 19",
+          apartment: "32",
           countryId: country.id,
           stateId: state.id,
           cityId: city.id,
-          zipCode: '10300',
-          phone: '1001001002',
-          dob: new Date('1990-01-02'),
-          ssn: '123456789',
-          dlid: '123456789',
-          imageUrl: '',
+          zipCode: "10300",
+          phone: "1001001002",
+          dob: new Date("1990-01-02"),
+          ssn: "123456789",
+          dlid: "123456789",
+          imageUrl: "",
           percentage: 0,
           createdById: user.id,
-          type: 'CUSTOMER',
-          notes: '',
+          type: "CUSTOMER",
+          notes: "",
         },
         {
-          code: '8940',
-          firstName: 'Maria',
-          middleName: 'Teresa',
-          lastNameOne: 'Gonzalez',
-          lastNameTwo: 'Perez',
-          address: 'Calle 31',
-          apartment: '33',
+          code: "8940",
+          firstName: "Maria",
+          middleName: "Teresa",
+          lastNameOne: "Gonzalez",
+          lastNameTwo: "Perez",
+          address: "Calle 31",
+          apartment: "33",
           countryId: country.id,
           stateId: state.id,
           cityId: city.id,
-          zipCode: '10400',
-          phone: '1001001003',
-          dob: new Date('1990-01-03'),
-          ssn: '123456789',
-          dlid: '123456789',
-          imageUrl: '',
+          zipCode: "10400",
+          phone: "1001001003",
+          dob: new Date("1990-01-03"),
+          ssn: "123456789",
+          dlid: "123456789",
+          imageUrl: "",
           percentage: 0,
           createdById: user.id,
-          type: 'CUSTOMER',
-          notes: '',
+          type: "CUSTOMER",
+          notes: "",
         },
         {
-          code: '8950',
-          firstName: 'Javier',
-          middleName: '',
-          lastNameOne: 'Maroto',
-          lastNameTwo: 'Pacheco',
-          address: 'Calle 44',
-          apartment: '53',
+          code: "8950",
+          firstName: "Javier",
+          middleName: "",
+          lastNameOne: "Maroto",
+          lastNameTwo: "Pacheco",
+          address: "Calle 44",
+          apartment: "53",
           countryId: country.id,
           stateId: state.id,
           cityId: city.id,
-          zipCode: '10300',
-          phone: '1001001005',
-          dob: new Date('1990-01-05'),
-          ssn: '123456789',
-          dlid: '123456789',
-          imageUrl: '',
+          zipCode: "10300",
+          phone: "1001001005",
+          dob: new Date("1990-01-05"),
+          ssn: "123456789",
+          dlid: "123456789",
+          imageUrl: "",
           percentage: 0,
           createdById: user.id,
-          type: 'RECIPIENT',
-          notes: '',
-        }
-
+          type: "RECIPIENT",
+          notes: "",
+        },
       ],
     });
 
@@ -200,22 +213,21 @@ async function main() {
           name: "Cash Check",
           description: "Cash Check",
           createdById: user.id,
-          percentage: 0.5    //5%
+          percentage: 0.5, //5%
         },
         {
           name: "Money Order",
           description: "Money Order",
           createdById: user.id,
-          percentage: 0.49    //0.49
-        }
-      ]
-    })
+          percentage: 0.49, //0.49
+        },
+      ],
+    });
 
     const customer = await tx.customer.findMany();
     const checkTransactionType = await tx.checkTransactionType.findFirst();
 
     if (customer.length && checkTransactionType) {
-
       await tx.checkTransaction.createMany({
         data: [
           {
@@ -224,7 +236,7 @@ async function main() {
             number: "10001",
             amount: 1000,
             feed: 100,
-            createdById: user.id
+            createdById: user.id,
           },
           {
             customerId: customer[0].id,
@@ -232,7 +244,7 @@ async function main() {
             number: "10002",
             amount: 2000,
             feed: 200,
-            createdById: user.id
+            createdById: user.id,
           },
           {
             customerId: customer[0].id,
@@ -240,11 +252,10 @@ async function main() {
             number: "10003",
             amount: 3000,
             feed: 300,
-            createdById: user.id
-          }
-        ]
-      })
-
+            createdById: user.id,
+          },
+        ],
+      });
     }
 
     const company = await tx.company.findFirst();
@@ -258,7 +269,7 @@ async function main() {
             companyId: company.id,
             amount: 1000,
             feed: 100,
-            createdById: user.id
+            createdById: user.id,
           },
           {
             customerId: customer[0].id,
@@ -266,9 +277,9 @@ async function main() {
             companyId: company.id,
             amount: 500,
             feed: 50,
-            createdById: user.id
-          }
-        ]
+            createdById: user.id,
+          },
+        ],
       });
     }
   });
